5b678db954d8f0f1b22710d6f0881bab3491c48b
Suraj Ganeshan <suraj_g93@hotmail.com>
2015-11-25 01:09:26 +1300

    Fixed the reply system for each post, need to add submitting new replies for the paticular FeedBackPost

--- Nandoso/Nandoso/Scripts/Feedback.js
+++ Nandoso/Nandoso/Scripts/Feedback.js
@@ -1,4 +1,5 @@
-﻿document.addEventListener("DOMContentLoaded", function () {
+﻿var FeedbackListLength = 0;
+document.addEventListener("DOMContentLoaded", function () {
     console.log("DOM loaded and to go !");
     getFeedback();
 });
@@ -18,7 +19,9 @@ function getFeedback() {
 function setupFeedbackTable(FeedbackList) {
     var MenuTable = document.getElementById("FeedBackList");
     MenuTable.innerHTML = null;
-    for (i = FeedbackList.length-1;i >= 0; i--) {
+    FeedbackListLength = FeedbackList.length - 1;
+    console.log("FBL : " + FeedbackListLength)
+    for (i = FeedbackListLength; i >= 0; i--) {
         var row = document.createElement("tr");
 
         var userNameCol = document.createElement("td");
@@ -28,7 +31,33 @@ function setupFeedbackTable(FeedbackList) {
         var postCol = document.createElement("td");
         postCol.innerHTML = FeedbackList[i].post;
         row.appendChild(postCol);
-        getReplies(FeedbackList[i].id,row);
+
+        var viewRepliesCol = document.createElement("button");
+        viewRepliesCol.innerHTML = "Hello"
+        viewRepliesCol.setAttribute("id", FeedbackList[i].id);
+        viewRepliesCol.setAttribute("onclick", "storeFeedBackPost(this.id)")
+        row.appendChild(viewRepliesCol);
         MenuTable.appendChild(row);
+    }
+}
+
+function storeFeedBackPost(buttonID) {
+    console.log(buttonID);
+    if (typeof (Storage) !== "undefined") {
+        var reply = document.getElementById("FeedBackList");
+        if (FeedbackListLength + 1 == buttonID) {
+            sessionStorage.setItem("UserName", reply.rows[0].cells[0].innerHTML);
+            sessionStorage.setItem("Message", reply.rows[0].cells[1].innerHTML);
+            sessionStorage.setItem("postID", buttonID);
+            window.location.href = "replies.html";
+        } else {
+            sessionStorage.setItem("UserName", reply.rows[buttonID].cells[0].innerHTML);
+            sessionStorage.setItem("Message", reply.rows[buttonID].cells[0].innerHTML);
+            sessionStorage.setItem("postID", buttonID);
+            window.location.href = "replies.html";
+        }
+
+    } else {
+        document.getElementById("result").innerHTML = "Sorry, your browser does not support web storage...";
     }
 }
--- Nandoso/Nandoso/Scripts/FeedbackReply.js
+++ Nandoso/Nandoso/Scripts/FeedbackReply.js
@@ -1,4 +1,9 @@
-﻿function submitReply() {
+﻿document.addEventListener("DOMContentLoaded", function () {
+    console.log("DOM loaded and to go !");
+    getReplies(sessionStorage.getItem("postID"));
+});
+
+function submitReply() {
     var postData = {
         "userName":"Suraj",
         "post":document.getElementById("FeedbackText").value
@@ -6,17 +11,25 @@
     postFeedback(getFeedback, postData);
 }
 
-function getReplies(postID,row) {
-    FeedbackReplyModel.getFeedback(setupFeedbackReplyTable, postID,row);
+function getReplies(postID) {
+    FeedbackReplyModel.getFeedback(setupFeedbackReplyTable, postID);
 }
 
-function setupFeedbackReplyTable(ReplyList, postID,row) {
- //   var MenuTable = document.getElementById("FeedBackList");
+function setupFeedbackReplyTable(ReplyList, postID) {
+    var MenuTable = document.getElementById("ReplyList");
     for (i = ReplyList.length - 1; i >= 0; i--) {
         if (postID == ReplyList[i].postID) {
-            var ReplyCol = document.createElement("td");
-            ReplyCol.innerHTML = ReplyList[i].userName + ":   " + ReplyList[i].reply;
-            row.appendChild(ReplyCol);
+            var row = document.createElement("tr");
+
+            var userNameCol = document.createElement("td");
+            userNameCol.innerHTML = ReplyList[i].userName;
+            row.appendChild(userNameCol);
+
+            var replyCol = document.createElement("td");
+            replyCol.innerHTML = ReplyList[i].reply;
+            row.appendChild(replyCol);
+            MenuTable.appendChild(row);
         }
     }
 }
+
