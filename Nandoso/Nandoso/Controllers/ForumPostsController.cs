e42a2e7fea4de5fe91753afefaf01be4d60ab46c
SurajG1993 <suraj_g93@hotmail.com>
2015-11-24 21:37:23 +1300

    Added the FoumPost model and controller, was missing from the project

---
+++ Nandoso/Nandoso/Controllers/ForumPostsController.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using Nandoso.Models;
+
+namespace Nandoso.Controllers
+{
+    public class ForumPostsController : ApiController
+    {
+        private NandosoContext db = new NandosoContext();
+
+        // GET: api/ForumPosts
+        public IQueryable<ForumPost> GetForumPosts()
+        {
+            return db.ForumPosts;
+        }
+
+        // GET: api/ForumPosts/5
+        [ResponseType(typeof(ForumPost))]
+        public IHttpActionResult GetForumPost(int id)
+        {
+            ForumPost forumPost = db.ForumPosts.Find(id);
+            if (forumPost == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(forumPost);
+        }
+
+        // PUT: api/ForumPosts/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutForumPost(int id, ForumPost forumPost)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != forumPost.id)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(forumPost).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ForumPostExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/ForumPosts
+        [ResponseType(typeof(ForumPost))]
+        public IHttpActionResult PostForumPost(ForumPost forumPost)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.ForumPosts.Add(forumPost);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = forumPost.id }, forumPost);
+        }
+
+        // DELETE: api/ForumPosts/5
+        [ResponseType(typeof(ForumPost))]
+        public IHttpActionResult DeleteForumPost(int id)
+        {
+            ForumPost forumPost = db.ForumPosts.Find(id);
+            if (forumPost == null)
+            {
+                return NotFound();
+            }
+
+            db.ForumPosts.Remove(forumPost);
+            db.SaveChanges();
+
+            return Ok(forumPost);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool ForumPostExists(int id)
+        {
+            return db.ForumPosts.Count(e => e.id == id) > 0;
+        }
+    }
+}
---
+++ Nandoso/Nandoso/Models/ForumPost.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+using Newtonsoft.Json;
+namespace Nandoso.Models
+{
+    public class ForumPost
+    {
+        [Key]
+        public int id { get; set; }
+        public string userName { get; set; }
+        public string post { get; set; }
+
+        [JsonIgnore]
+        public virtual ICollection<ForumReplies> reply { get; set; } // Relational data to forum topic
+    }
+}
